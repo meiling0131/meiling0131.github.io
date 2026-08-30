270783b1572bdfc1bb2e5b8f940e2a9886c21166
meiling0131 <meiling.wang0131@gmail.com>
2026-08-30 03:29:02 +0000

    Deploying to gh-pages from @ meiling0131/meiling0131.github.io@8252b6c3daf94100316320c09678a1f7627c2c04 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Undergraduate in Computer Science at Purdue, working on ML systems and efficient LLM inference. Download the PDF with the icon on the right.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
